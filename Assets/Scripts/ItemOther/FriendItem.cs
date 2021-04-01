8322740859a9c92b912c7588524d86cd390fd6ef
aa1345171901 <32924652+1345171901@users.noreply.github.com>
2021-04-01 16:38:51 +0800

    add panel
    add some bg and three panel of friend detail, myself detail,modify myself detail panel

---
+++ Assets/Scripts/ItemOther/FriendItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendItem : MonoBehaviour
+{
+    private Button friendBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 获取组件
+        friendBtn = GetComponent<Button>();
+
+        // 设置事件
+        friendBtn.onClick.AddListener(BtnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 按钮点击
+    /// </summary>
+    private void BtnClick()
+    {
+        Facade.Instance.PushPanel(UIPanelType.ChatPanel);
+    }
+}
