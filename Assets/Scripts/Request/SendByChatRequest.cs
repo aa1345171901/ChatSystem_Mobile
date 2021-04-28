8df9ed01570a44af37304731fb8c4e759afacf7b
aa1345171901 <32924652+1345171901@users.noreply.github.com>
2021-04-28 23:24:28 +0800

    add send by chat request

---
+++ Assets/Scripts/Request/SendByChatRequest.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendByChatRequest : BaseRequest
+{
+    private ChatPanel chatPanel;
+
+    public override void Awake()
+    {
+        chatPanel = GetComponent<ChatPanel>();
+        requestCode = RequestCode.Message;
+        actionCode = ActionCode.SendByChat;
+        base.Awake();
+    }
+}
