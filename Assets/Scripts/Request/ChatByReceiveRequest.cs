93edeecad7e2e6d6d807e58fd5c38e58ce2b6643
aa1345171901 <32924652+1345171901@users.noreply.github.com>
2021-04-29 23:11:34 +0800

    add chat by receive

---
+++ Assets/Scripts/Request/ChatByReceiveRequest.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatByReceiveRequest : BaseRequest
+{
+    private ChatPanel chatPanel;
+
+    public override void Awake()
+    {
+        chatPanel = GetComponent<ChatPanel>();
+        requestCode = RequestCode.Message;
+        actionCode = ActionCode.ChatByReceive;
+        base.Awake();
+    }
+}
